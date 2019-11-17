0f6d9c5d0692b1e3c17d4a6ed91a51b171aea661
Jota-Wolf <lobos.5769>
2019-11-17 13:43:32 -0300

    arreglo de filtros pra pythonanywhere

--- core/static/core/js/filtros.js
+++ core/static/core/js/filtros.js
@@ -2,26 +2,44 @@ $(document).ready(function() {
 
     var baseUrlcasas = 'http://jotawolf.pythonanywhere.com/casas/';
     var fil_cas_tipo_contacto = $('#fil_cas_tipo_contacto');
+    var fil_cas_habitaciones = $('#fil_cas_habitaciones');
 
     var baseUrldeptos = 'http://jotawolf.pythonanywhere.com/departamento/';
     var fil_depto_tipo_contacto = $('#fil_depto_tipo_contacto');
+    var fil_depto_habitaciones = $('#fil_depto_habitaciones');
 
     var baseUrlterres = 'http://jotawolf.pythonanywhere.com/terrenos/';
     var fil_terr_tipo_contacto = $('#fil_terr_tipo_contacto');
+    var fil_terr_ciudad = $('#fil_terr_ciudad');
 
     $(fil_cas_tipo_contacto).change(function() {
         var fil_cas_tipo_contacto = $(this).val();
-        window.location.href = baseUrlcasas + '?filter=' + fil_cas_tipo_contacto;
+        window.location.href = baseUrlcasas + '?filtro_contacto=' + fil_cas_tipo_contacto;
+    });
+
+    $(fil_cas_habitaciones).change(function() {
+        var fil_cas_habitaciones = $(this).val();
+        window.location.href = baseUrlcasas + '?filtro_habitaciones=' + fil_cas_habitaciones;
     });
 
     $(fil_depto_tipo_contacto).change(function() {
         var fil_depto_tipo_contacto = $(this).val();
-        window.location.href = baseUrldeptos + '?filter=' + fil_depto_tipo_contacto;
+        window.location.href = baseUrldeptos + '?filtro_contacto=' + fil_depto_tipo_contacto;
+    });
+
+    $(fil_depto_habitaciones).change(function() {
+        var fil_depto_habitaciones = $(this).val();
+        window.location.href = baseUrldeptos + '?filtro_habitaciones=' + fil_depto_habitaciones;
     });
 
     $(fil_terr_tipo_contacto).change(function() {
         var fil_terr_tipo_contacto = $(this).val();
-        window.location.href = baseUrlterres + '?filter=' + fil_terr_tipo_contacto;
+        window.location.href = baseUrlterres + '?filtro_contacto=' + fil_terr_tipo_contacto;
+    });
+
+    $(fil_terr_ciudad).change(function() {
+        var fil_terr_ciudad = $(this).val();
+        window.location.href = baseUrlterres + '?filtro_ciudad=' + fil_terr_ciudad;
     });
 
 });
